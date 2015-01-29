1b640705e092b40a0b49cc8766f8fda2c2ea2823
knightliao <knightliao@gmail.com>
2015-01-29 14:41:46 +0800

    re

---
+++ src/main/java/com/knightliao/jobflow/webdemo/HomeController.java
@@ -0,0 +1,44 @@
+package com.knightliao.jobflow.webdemo;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.knightliao.jobflow.webdemo.service.Message;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    AmqpTemplate amqpTemplate;
+
+    @RequestMapping(value = "/")
+    public String home(Model model) {
+        model.addAttribute(new Message());
+        return "WEB-INF/views/home.jsp";
+    }
+
+    @RequestMapping(value = "/publish", method = RequestMethod.POST)
+    public String publish(Model model, Message message) {
+        // Send a message to the "messages" queue
+        amqpTemplate.convertAndSend("foo.bar", message.getValue());
+        model.addAttribute("published", true);
+        return home(model);
+    }
+
+    @RequestMapping(value = "/get", method = RequestMethod.POST)
+    public String get(Model model) {
+        // Receive a message from the "messages" queue
+        String message = (String) amqpTemplate.receiveAndConvert("messages");
+        if (message != null) {
+            model.addAttribute("got", message);
+        } else {
+            model.addAttribute("got_queue_empty", true);
+        }
+
+        return home(model);
+    }
+}
---
+++ src/main/java/com/knightliao/jobflow/webdemo/service/Message.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ */
+package com.knightliao.jobflow.webdemo.service;
+
+/**
+ * Created by knightliao on 15/1/29.
+ */
+public class Message {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
